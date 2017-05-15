41ffd38e945ed77a60a19ad7ef670936dcd294ce
Erica Rodriguez <erica.rodriguez.mail@gmail.com>
2017-05-14 23:46:29 -0500

    second try

---
+++ keys.js
@@ -0,0 +1,9 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'rfhhSypVSI5lRDvF7G6f6ZXAQ',
+  consumer_secret: '2gOfHYFYj52ZScphB8MaKTiIZx64Umu1b7wfKMc0EJkp6GM6cy',
+  access_token_key: '862854258949074945-ytm1F0B5afNWw9Rg54YT7TMyfwdCoSn',
+  access_token_secret: 'nGox9XCEVVhGgvkIYd5AoNT5n41pf7QuEmsY8wAErVfLK',
+}
+
---
+++ liri.js
@@ -0,0 +1,38 @@
+var keys = require('./keys.js').twitterKeys;
+
+var Twitter = require('twitter');
+
+var command = process.argv[2];
+//console.log(process.argv);
+
+switch (command ){
+    case 'my-tweets':
+    //console.log("test this crap");
+        var client = new Twitter(keys);
+        var params = {screen_name: 'ericacodecamper', count: 20};
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+    if (!error) {
+        tweets.forEach(function(tweet) {
+            console.log(tweet.text);
+        })
+    }
+    });
+    break;
+
+    case 'spotify-this-song':
+    console.log("test this crap again");
+    break;
+
+case 'movie-this':
+    console.log("test this crap again again");
+    break;
+
+    case 'do-what-it-says':
+    console.log("test this crap last");
+    break;
+}
+
+
+
+
+
